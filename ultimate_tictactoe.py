8be8039c10867e0212181565d98772f66c21d99c
Ask Hetland Berentsen <askberentsen@gmail.com>
2022-03-28 18:49:36 +0200

    Re-added cloning to constructor
    default values were being mutated

--- ultimate_tictactoe.py
+++ ultimate_tictactoe.py
@@ -7,9 +7,9 @@ class UltimateTicTacToe:
     
     
     def __init__(self, board=np.zeros(UTTT_SHAPE), select=np.ones(TTT_SHAPE), wins=np.zeros(TTT_SHAPE), player=1, turn=0):
-        self.board = board
-        self.select = select
-        self.wins = wins
+        self.board = np.copy(board)
+        self.select = np.copy(select)
+        self.wins = np.copy(wins)
         if player not in (-1, 1):
             raise ValueError(f"Player must be 1 (X) or -1 (O), player was {player}")
         self.player = player #x starts
@@ -17,7 +17,7 @@ class UltimateTicTacToe:
     
     
     def immutable_transition_function(self, action : tuple):
-        game_copy = UltimateTicTacToe(np.copy(self.board), np.copy(self.select), np.copy(self.wins), self.player, self.turn)
+        game_copy = UltimateTicTacToe(self.board, self.select, self.wins, self.player, self.turn)
         winner = game_copy.mutable_transition_function(action)
         return game_copy, winner
     
